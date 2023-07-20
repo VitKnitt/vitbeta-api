bbaa6e9b0a3ca8e1bbf3cd7055d2755345607458
Vit <vit.spa@email.cz>
2023-07-20 12:14:17 +0200

    adjusting

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.post("/login", async (req, res) => {
       { expiresIn: '240h' },
       (err, token) => {
         if (err) throw err;
-        res.cookie("token", token, { maxAge: 10 * 24 * 60 * 60 * 1000, domain: "vitbeta.onrender.com", })
+        res.cookie("token", token, { maxAge: 10 * 24 * 60 * 60 * 1000 })
         console.log("Request Cookies:", req.cookies);
         res.status(200).json({ name, id: userDoc._id});
       }
