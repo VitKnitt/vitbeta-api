b2bd2563c97bf269639e7a54b838db2f614128e9
Vit <vit.spa@email.cz>
2023-07-28 14:14:03 +0200

    update

--- index.js
+++ index.js
@@ -50,10 +50,10 @@ app.use(express.static("public"));
 
 
 //app.use(cors({ origin: ['http://localhost:3000', 'https://vitbeta.onrender.com', 'https://www.edgetale.com'], credentials: true }))
-//app.use(cors({ origin: 'https://vitbeta.onrender.com', credentials: true }))
+app.use(cors({ origin: 'https://vitbeta.onrender.com', credentials: true }))
 
 
-
+/*
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   const allowedOrigins = ['http://localhost:3000', 'https://vitbeta.onrender.com', 'https://www.edgetale.com'];
@@ -66,7 +66,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
   next();
 });
-
+*/
 
 
 /*
