740b02d84653a0865970a85a0e907d315da298f9
Vit <vit.spa@email.cz>
2023-07-29 13:20:48 +0200

    update

--- routes/userActions/login.js
+++ routes/userActions/login.js
@@ -21,7 +21,8 @@ router.post("/", async (req, res) => {
         (err, token) => {
           if (err) return res.status(500).json("internal error");
          
-          res.cookie("token", token, { maxAge: 10 * 24 * 60 * 60 * 1000}).status(200).json({ name, id: userDoc._id});
+          //res.cookie("token", token, { maxAge: 10 * 24 * 60 * 60 * 1000}).status(200).json({ name, id: userDoc._id});
+          res.status(200).json({token,name,id: userDoc._id})
         }
       );
     } else {
